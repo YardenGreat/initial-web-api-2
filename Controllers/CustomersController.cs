45f94f4ca7cc439e0b95862f2f95b93d32e87de1
Yarden <yarden2100@gmail.com>
2018-06-15 10:08:38 +0300

    First commit

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        Customer[] customers = new Customer[]
+        {
+            new Customer{Id = 1, FirstName = "Larry", LastName= "Jenkins"},
+            new Customer{Id = 2, FirstName = "Jenny", LastName= "Miller"},
+            new Customer{Id = 3, FirstName = "Mike", LastName= "Tyson"}
+        };
+
+        [Route("")]
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return customers;
+        }
+
+        // The parameter from the url and the parameter received by the action must have the same name
+        [Route("getSpecificCustomer/{id}")]
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault((p) => p.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+    }
+}
